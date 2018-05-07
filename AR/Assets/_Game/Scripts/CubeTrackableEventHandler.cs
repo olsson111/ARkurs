a6d245bcf494db41fe61b3241c97878c15f3bcd3
olsson111 <38218795+olsson111@users.noreply.github.com>
2018-05-07 14:41:07 +0200

    Grundversion

---
+++ AR/Assets/_Game/Scripts/CubeTrackableEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CubeTrackableEventHandler : DefaultTrackableEventHandler {
+
+    //Used to override the default eventhandler
+    protected override void OnTrackingFound()
+    {
+        Debug.Log("Hej");
+
+
+        //All of the original method
+        base.OnTrackingFound();
+    }
+
+    //Used to override the default eventhandler
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+    }
+}
